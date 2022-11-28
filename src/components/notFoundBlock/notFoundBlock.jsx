91d45dcbb6c0c9cb7e4c2b25fabd6f9c8c70b324
tr1ble <artsiom_sadouski@epam.com>
2022-11-28 17:20:43 +0300

    EPMRPP-80453 || Code review fixes - 5

--- src/components/notFoundBlock/notFoundBlock.jsx
+++ src/components/notFoundBlock/notFoundBlock.jsx
@@ -19,25 +19,23 @@ import { PageMetadata } from '@docusaurus/theme-common';
 import Layout from '@theme/Layout';
 import './notFoundBlock.css';
 
-export const NotFoundBlock = () => {
-    return (
-        <>
-            <PageMetadata title='Page Not Found' />
-            <Layout>
-                <div className={'not-found-content'}>
-                    <div className={'not-found-img'} />
-                    <h1 className={'not-found-title'}>
-                        Page Not Found
-                    </h1>
-                    <p className={'not-found-text'}>
-                        You may have used an outdated link as there have been some changes in the structure of the
-                        documentation.
-                    </p>
-                    <p className={'not-found-text'}>
-                        But it's clearly here somewhere. <br/> Use the keyword search to find!
-                    </p>
-                </div>
-            </Layout>
-        </>
-    );
-}
+export const NotFoundBlock = () =>
+    <>
+        <PageMetadata title='Page Not Found' />
+        <Layout>
+            <div className={'not-found-content'}>
+                <div className={'not-found-img'} />
+                <h1 className={'not-found-title'}>
+                    Page Not Found
+                </h1>
+                <p className={'not-found-text'}>
+                    You may have used an outdated link as there have been some changes in the structure of the
+                    documentation.
+                </p>
+                <p className={'not-found-text'}>
+                    But it's clearly here somewhere. <br/> Use the keyword search to find!
+                </p>
+            </div>
+        </Layout>
+    </>
+
