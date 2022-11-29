acf7ad9ca5eb5d48a9cdabcb4b7e1eb599621e64
tr1ble <43150561+tr1ble@users.noreply.github.com>
2022-11-29 10:05:27 +0300

    EPMRPP-80453 || Not found 404 page (#459)
    * EPMRPP-80453 || Not found 404 page
    
    * EPMRPP-80453 || Code style fixes
    
    * EPMRPP-80453 || Mobile view fix
    
    * EPMRPP-80453 || Style fixes after discuss
    
    * EPMRPP-80453 || Mobile view var fix
    
    * EPMRPP-80453 || Extra max-width fix
    
    * EPMRPP-80453 || Code review fixes - 1
    
    * EPMRPP-80453 || Code review fixes - 2
    
    * EPMRPP-80453 || Code review fixes - 3
    
    * EPMRPP-80453 || Code review fixes - 4
    
    * EPMRPP-80453 || Code review fixes - 5
    
    * EPMRPP-80453 || Code review fixes - 6

---
+++ src/components/notFoundBlock/notFoundBlock.jsx
@@ -0,0 +1,42 @@
+/*
+ * Copyright 2022 EPAM Systems
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import React from 'react';
+import { PageMetadata } from '@docusaurus/theme-common';
+import Layout from '@theme/Layout';
+import './notFoundBlock.css';
+
+export const NotFoundBlock = () =>
+    <>
+        <PageMetadata title='Page Not Found' />
+        <Layout>
+            <div className={'not-found-content'}>
+                <div className={'not-found-img'} />
+                <h1 className={'not-found-title'}>
+                    Page Not Found
+                </h1>
+                <p className={'not-found-text'}>
+                    You may have used an outdated link as there have been some changes in the structure of the
+                    documentation.
+                </p>
+                <p className={'not-found-text'}>
+                    But it's clearly here somewhere. <br/> Use the keyword search to find!
+                </p>
+            </div>
+        </Layout>
+    </>
+;
+
---
+++ src/theme/NotFound.js
@@ -0,0 +1,4 @@
+import React from 'react';
+import { NotFoundBlock } from '../components/notFoundBlock';
+
+export default () => <NotFoundBlock />;
