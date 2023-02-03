d6f4364cc50dfb035d7f321d4616cff50685b46b
Ilya Hancharyk <Ilya_Hancharyk@epam.com>
2023-02-03 13:55:15 +0300

    EPMRPP-81667 || Update url & baseUrl

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -7,8 +7,8 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 /** @type {import('@docusaurus/types').Config} */
 const config = {
   title: 'Report Portal Documentation',
-  url: 'https://documentation.reportportal.io',
-  baseUrl: '/',
+  url: 'https://reportportal.io',
+  baseUrl: '/docs/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
