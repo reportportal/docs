515b031a6fc93899d5480734317549065b34e64c
Yuliya_Prihodko <Yuliya_Prihodko@epam.com>
2025-05-30 14:53:01 +0300

    EPMRPP-103858 || Update File Storage options doc

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -439,7 +439,7 @@ const config = {
             from: '/installation-steps/MigrateOneInstanceToAnother',
           },
           {
-            to: '/installation-steps-advanced/FileStorageOptions',
+            to: '/installation-steps-advanced/file-storage-options/',
             from: '/installation-steps/ReportPortal23.1FileStorageOptions',
           },
           {
